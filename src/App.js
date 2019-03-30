28cc5d17939e4a27d9cf8b61ceb362e8639ff475
Luka <lukam@nym.hush.com>
2019-03-30 23:33:20 +0100

    Pomodoro Clock - Start build
    1.  build 23;00 -> 23:33
    
    Deleted the favicon and cleaned up the create-react-app initial files.
    
    Prepared for the fCC user stories 1 / 8
    
    Started styling. No logic, except time display research in comments.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// time = 1500
+// var minutes = Math.floor(time / 60);
+// var seconds = time - minutes * 60;
+/*
+function fancyTimeFormat(time)
+{   
+    // Hours, minutes and seconds
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = ~~time % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+*/
+const sessions = [
+  {
+    name: "session",
+    iniLen: 1500,
+    len: 0,
+  },
+  {
+    name: "break",
+    iniLen: 300,
+    len: 0
+  }
+]
+class Timer extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="text-center">
+        <div id={`${this.props.name}-label`} className="text-center text-white label"><h4>{this.props.name} length</h4></div>
+        <div className="center text-center text-white"><h4>{this.props.iniLen/60}:00</h4></div>
+        {
+          // napisat funkcijo handleMinus, handlePlus za odštevanje in seštevanje minut
+          // plafon je props.sessionLen pa props.breakLen
+        }
+        <button className="left" onClick={this.handleMinus} id={`${this.props.name}-decrement`}><i className="fas fa-minus"></i></button>
+        <button className="right" onClick={this.handlePlus} id={`${this.props.name}-increment`}><i className="fas fa-plus"></i></button>
+      </div>
+    )
+  }
+}
+
+class Counter extends Component {
+  render() {
+    return(
+      <div id="" className="text-center counter-round">
+        <svg height="350" width="350">
+          <circle cx="175" cy="175" r="173" stroke="gray" stroke-width="3" fill="none" z-position="3" />
+        </svg>
+        <div id="timer-label" className="text-white font-weight-bold"><h2>Session</h2></div>
+        <div id="time-left" className="text-white font-weight-bold"><h2>25:00</h2></div>
+      </div>
+    )
+  }
+}
+
+class Pomodoro extends Component {
+  state = {
+    sessions: sessions,
+    maxSession: 60,
+    maxBreak: 60,
+  }
+  render() {
+    return (
+      <div className="main container">
+        <div className="grid">
+          {this.state.sessions.map(e => (
+          <Timer key={`key-${e.name}`} name={e.name} iniLen={e.iniLen}/>
+          
+          ))}
+        </div>
+        <Counter  />
       </div>
     );
   }
 }
 
-export default App;
+export default Pomodoro;
